3394bc9ff6fe546b7fd4a4effa5e76d8095e5770
bdevore17 <bdevore17@gmail.com>
2016-04-24 23:46:14 -0400

    demo version

--- roaster.ino
+++ roaster.ino
@@ -68,9 +68,9 @@ void setup()
   bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
   // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
   bluetooth.begin(9600);  // Start bluetooth serial at 9600
-  
+
   initSht(SHT_dataPin, SHT_clockPin);
-  
+
   servo.attach(servoPin);
   servo2.attach(servoPin2);
   pinMode(pin11, OUTPUT);
@@ -94,12 +94,12 @@ boolean lightOn = false;
 
 
 void readDS3231time(byte *second,
-byte *minute,
-byte *hour,
-byte *dayOfWeek,
-byte *dayOfMonth,
-byte *month,
-byte *year)
+                    byte *minute,
+                    byte *hour,
+                    byte *dayOfWeek,
+                    byte *dayOfMonth,
+                    byte *month,
+                    byte *year)
 {
   Wire.beginTransmission(DS3231_I2C_ADDRESS);
   Wire.write(0); // set DS3231 register pointer to 00h
@@ -115,26 +115,26 @@ byte *year)
   *year = bcdToDec(Wire.read());
 }
 void startTimer() {
-      readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,&year);
-      
-      readDS3231time(&second2, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,&year);
-      Serial.print(second2-second);
-      Serial.print(" Seconds");
+  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+
+  readDS3231time(&second2, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  Serial.print(second2 - second);
+  Serial.print(" Seconds");
 }
 
 // Convert normal decimal numbers to binary coded decimal
 byte decToBcd(byte val)
 {
-  return( (val/10*16) + (val%10) );
+  return ( (val / 10 * 16) + (val % 10) );
 }
 // Convert binary coded decimal to normal decimal numbers
 byte bcdToDec(byte val)
 {
-  return( (val/16*10) + (val%16) );
+  return ( (val / 16 * 10) + (val % 16) );
 }
 
 void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
-dayOfMonth, byte month, byte year)
+                   dayOfMonth, byte month, byte year)
 {
   // sets time and date data to DS3231
   Wire.beginTransmission(DS3231_I2C_ADDRESS);
@@ -152,16 +152,17 @@ dayOfMonth, byte month, byte year)
 
 void loop()
 {
-  
+
   if (bluetooth.available()) // If the bluetooth sent any characters
   {
-  
+
     int r = bluetooth.read();
     Serial.print((char)r);
     // Send any characters the bluetooth prints to the serial monitor
     if (r == 107) { // k character
-      rotateTowardsLight();
-      digitalWrite(6, HIGH);
+      baselineTemp = (getShtTemperature() * 9 / 5 + 32);
+      bluetooth.print("Baseline: ");
+      bluetooth.print(baselineTemp);
       lightOn = true;
     }
     else if (r == 108) {
@@ -181,26 +182,29 @@ void loop()
   readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
   Serial.println(second);
   if (lightOn) {
+    rotateTowardsLight();
+    digitalWrite(6, HIGH);
     rotateMarshmallow();
-    bluetooth.println(getShtTemperature()*9/5+32);
+    bluetooth.println(getShtTemperature() * 9 / 5 + 32);
     rotateAwayFromLight();
     digitalWrite(6, LOW);
-    Serial.print("Temperature: ");
+    bluetooth.print("Temperature: ");
     delay(7000);
-    baselineTemp = (getShtTemperature()*9/5+32);
-    Serial.println(getShtTemperature()*9/5+32);
+    Serial.println(getShtTemperature() * 9 / 5 + 32);
     //detect temperature
-    bluetooth.println(getShtTemperature()*9/5+32);
+    bluetooth.println(getShtTemperature() * 9 / 5 + 32);
     delay(7000);
-    newTemp = (getShtTemperature()*9/5+32);
-    bluetooth.println(getShtTemperature()*9/5+32);
-    lightOn = false;
-    if(newTemp - baselineTemp > .5) {
-    toasted();
+    newTemp = (getShtTemperature() * 9 / 5 + 32);
+    bluetooth.println(getShtTemperature() * 9 / 5 + 32);
+    if (newTemp - baselineTemp > 1.25) {
+      toasted();
+      lightOn = false;
     }
     else {
       Serial.println("Check setup.");
     }
+    bluetooth.print("Difference: ");
+    bluetooth.println(newTemp - baselineTemp);
   }
   //printShtTemperatureCelsius();
 }
@@ -211,7 +215,7 @@ void rotateAwayFromLight() {
     delay(15);
   }
   delay(50);
-//  servo2.write(115);
+  //  servo2.write(115);
 }
 
 void rotateTowardsLight() {
@@ -222,31 +226,31 @@ void rotateTowardsLight() {
   //  servo2.write(115);
 }
 void rotateMarshmallow() {
-  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,&year);
+  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
   Serial.println(second);
   do {
-  for (angle = 0; angle <= 180; angle++)
-  {
-    servo.write(angle);
-    delay(50);
-  }
-  // now scan back from 180 to 0 degrees
-  for (angle = 180; angle > 0; angle--)
-  {
-    servo.write(angle);
-    delay(50);
-  }
-  readDS3231time(&second2, &minute2, &hour, &dayOfWeek, &dayOfMonth, &month,&year);
-//  Serial.println(minute);
-//  Serial.println(minute2-minute);
-  Serial.println((double(minute2) + (double(second2) / 60.0)) - (double(minute) + (double(second) / 60.0)));
-  } while((double(minute2) + (double(second2) / 60.0)) - (double(minute) + (double(second) / 60.0)) < 1.5);
- // displayTime();
+    for (angle = 0; angle <= 180; angle++)
+    {
+      servo.write(angle);
+      delay(50);
+    }
+    // now scan back from 180 to 0 degrees
+    for (angle = 180; angle > 0; angle--)
+    {
+      servo.write(angle);
+      delay(50);
+    }
+    readDS3231time(&second2, &minute2, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+    //  Serial.println(minute);
+    //  Serial.println(minute2-minute);
+    Serial.println((double(minute2) + (double(second2) / 60.0)) - (double(minute) + (double(second) / 60.0)));
+  } while ((double(minute2) + (double(second2) / 60.0)) - (double(minute) + (double(second) / 60.0)) < 1.25);
+  // displayTime();
   Wire.endTransmission();
-//  Serial.print(minute2-minute);
-//  Serial.print("minutes and ");
-//  Serial.print(second2-second);
-//  Serial.print("seconds \n");
+  //  Serial.print(minute2-minute);
+  //  Serial.print("minutes and ");
+  //  Serial.print(second2-second);
+  //  Serial.print("seconds \n");
 }
 
 void displayTime()
@@ -254,18 +258,18 @@ void displayTime()
   byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
   // retrieve data from DS3231
   readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,
-  &year);
+                 &year);
   // send it to the serial monitor
   Serial.print(hour, DEC);
   // convert the byte variable to a decimal number when displayed
   Serial.print(":");
-  if (minute<10)
+  if (minute < 10)
   {
     Serial.print("0");
   }
   Serial.print(minute, DEC);
   Serial.print(":");
-  if (second<10)
+  if (second < 10)
   {
     Serial.print("0");
   }
@@ -277,54 +281,54 @@ void displayTime()
   Serial.print("/");
   Serial.print(year, DEC);
   Serial.print(" Day of week: ");
-  switch(dayOfWeek){
-  case 1:
-    Serial.println("Sunday");
-    break;
-  case 2:
-    Serial.println("Monday");
-    break;
-  case 3:
-    Serial.println("Tuesday");
-    break;
-  case 4:
-    Serial.println("Wednesday");
-    break;
-  case 5:
-    Serial.println("Thursday");
-    break;
-  case 6:
-    Serial.println("Friday");
-    break;
-  case 7:
-    Serial.println("Saturday");
-    break;
+  switch (dayOfWeek) {
+    case 1:
+      Serial.println("Sunday");
+      break;
+    case 2:
+      Serial.println("Monday");
+      break;
+    case 3:
+      Serial.println("Tuesday");
+      break;
+    case 4:
+      Serial.println("Wednesday");
+      break;
+    case 5:
+      Serial.println("Thursday");
+      break;
+    case 6:
+      Serial.println("Friday");
+      break;
+    case 7:
+      Serial.println("Saturday");
+      break;
   }
 }
 
 
-void initSht(int dataPin, int clockPin){
+void initSht(int dataPin, int clockPin) {
   SHT_clockPin = clockPin;
   SHT_dataPin = dataPin;
 }
 
 /*
- * Print the temperature in Celsius
- */
-void printShtTemperatureCelsius(){
+   Print the temperature in Celsius
+*/
+void printShtTemperatureCelsius() {
   //these can take a bit to get the values (100ms or so)
   float temperature = 0;
   temperature = getShtTemperature();
-  
-//  Serial.print("Temperature: ");
-//  Serial.print(temperature);
-//  Serial.println(" C");
+
+  //  Serial.print("Temperature: ");
+  //  Serial.print(temperature);
+  //  Serial.println(" C");
 }
 
 /*
- * Print the temperature in Fahrenheit
- */
-void printShtTemperatureFahrenheit(){
+   Print the temperature in Fahrenheit
+*/
+void printShtTemperatureFahrenheit() {
   //these can take a bit to get the values (100ms or so)
   float temperature = 0;
   temperature = getShtTemperature();
@@ -336,9 +340,9 @@ void printShtTemperatureFahrenheit(){
 }
 
 /*
- * Print the humidity percentage
- */
-void printShtHumidity(){
+   Print the humidity percentage
+*/
+void printShtHumidity() {
   float humidity = 0;
   getShtHumidity(&humidity);
   Serial.print("Humidity: ");
@@ -347,39 +351,39 @@ void printShtHumidity(){
 }
 
 /*
- * Retrieve the temperature in Celsius and place in the
- * passed variable
- */
-float getShtTemperature(){
+   Retrieve the temperature in Celsius and place in the
+   passed variable
+*/
+float getShtTemperature() {
   //Return Temperature in Celsius
   SHT_sendCommand(B00000011, SHT_dataPin, SHT_clockPin);
   SHT_waitForResult(SHT_dataPin);
 
   int val = SHT_getData(SHT_dataPin, SHT_clockPin);
   SHT_skipCrc(SHT_dataPin, SHT_clockPin);
-  
+
   float temp = (float)val * 0.01 - 40; //convert to celsius
   return temp;
 }
 
 /*
- * Retrieve the humidity and place in the
- * passed variable
- */
-void getShtHumidity(float* humid){
+   Retrieve the humidity and place in the
+   passed variable
+*/
+void getShtHumidity(float* humid) {
   //Return  Relative Humidity
   SHT_sendCommand(B00000101, SHT_dataPin, SHT_clockPin);
   SHT_waitForResult(SHT_dataPin);
   int val = SHT_getData(SHT_dataPin, SHT_clockPin);
   SHT_skipCrc(SHT_dataPin, SHT_clockPin);
-  
-  *humid = -4.0 + 0.0405 * val + -0.0000028 * val * val; 
+
+  *humid = -4.0 + 0.0405 * val + -0.0000028 * val * val;
 }
 
 /*
- * Bitbanging for issuing a command to the SHT15
- */
-void SHT_sendCommand(int command, int dataPin, int clockPin){
+   Bitbanging for issuing a command to the SHT15
+*/
+void SHT_sendCommand(int command, int dataPin, int clockPin) {
   // send a command to the SHTx sensor
   // transmission start
   pinMode(dataPin, OUTPUT);
@@ -406,16 +410,16 @@ void SHT_sendCommand(int command, int dataPin, int clockPin){
 
 
 /*
- * Waiting for ACK from the SHT15
- */
-void SHT_waitForResult(int dataPin){
+   Waiting for ACK from the SHT15
+*/
+void SHT_waitForResult(int dataPin) {
   // wait for the SHTx answer
   pinMode(dataPin, INPUT);
 
   int ack; //acknowledgement
 
   //need to wait up to 2 seconds for the value
-  for (int i = 0; i < 1000; ++i){
+  for (int i = 0; i < 1000; ++i) {
     delay(2);
     ack = digitalRead(dataPin);
     if (ack == LOW) break;
@@ -425,9 +429,9 @@ void SHT_waitForResult(int dataPin){
 }
 
 /*
- * Bitbanging for reading data from the SHT15
- */
-int SHT_getData(int dataPin, int clockPin){
+   Bitbanging for reading data from the SHT15
+*/
+int SHT_getData(int dataPin, int clockPin) {
   // get data from the SHTx sensor
 
   // get the MSB (most significant bits)
@@ -449,9 +453,9 @@ int SHT_getData(int dataPin, int clockPin){
 }
 
 /*
- * CRC check
- */
-void SHT_skipCrc(int dataPin, int clockPin){
+   CRC check
+*/
+void SHT_skipCrc(int dataPin, int clockPin) {
   // skip CRC data from the SHTx sensor
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
